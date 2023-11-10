b02cc1366c94bae91a273ef12908eab22306e9ca
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2023-11-10 20:00:39 +0200

    Add HTML, CSS and Home Page

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+// import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+import Header from "./components/Header/Header";
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+  <>
+<Header />
+
+<Routes>
+  <Route path='/' element = {<Home />}/>
+</Routes>
+
+  </>
+  );
+}
+
+export default App;
