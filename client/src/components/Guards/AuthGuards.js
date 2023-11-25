b3e7cb68e207e0ba8b2e44a2b5c509c9325e8e78
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2023-11-25 18:11:17 +0200

    Update AuthGuards.js

--- client/src/components/Guards/AuthGuards.js
+++ client/src/components/Guards/AuthGuards.js
@@ -1,6 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthContext } from "../../contexts/AuthContext";
 
+//трябва да измисля owner guards - логнат потребител да не може да влиза в edit, delete?
+
 export default function AuthGuards({
     children
 }){
